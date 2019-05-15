49d22b915e6f2ede427f66976d457333696223c1
Colin Grambow <cgrambow@mit.edu>
2019-05-15 16:46:59 -0400

    Implement ReactionModel
    Write ReactionModel which encodes a batch of reactant and product
    molecules, forms the difference between their atom embeddings and feeds
    that to a difference encoder, before going through a final set of
    fully-connected layers.
    
    Restructure MoleculeModel so that ReactionModel and MoleculeModel both
    inherit from the Model base class because they share the same structure
    and the fully-connected network implementation.

--- chemprop/models/model.py
+++ chemprop/models/model.py
@@ -1,21 +1,25 @@
 from argparse import Namespace
+from typing import List, Union
 
+import torch
 import torch.nn as nn
+import numpy as np
 
-from .mpn import MPN
+from .mpn import MPN, MPNDiff
+from chemprop.features import BatchMolGraph
 from chemprop.nn_utils import get_activation_function, initialize_weights
 
 
-class MoleculeModel(nn.Module):
-    """A MoleculeModel is a model which contains a message passing network following by feed-forward layers."""
+class Model(nn.Module):
+    """Base class for molecule and reaction models, which only differ in their encoding."""
 
     def __init__(self, classification: bool):
         """
-        Initializes the MoleculeModel.
+        Initializes the Model.
 
         :param classification: Whether the model is a classification model.
         """
-        super(MoleculeModel, self).__init__()
+        super(Model, self).__init__()
 
         self.classification = classification
         if self.classification:
@@ -23,11 +27,11 @@ class MoleculeModel(nn.Module):
 
     def create_encoder(self, args: Namespace):
         """
-        Creates the message passing encoder for the model.
+        Creates the encoder for the model.
 
-        :param args: Arguments.
+        Should be overridden by all subclasses.
         """
-        self.encoder = MPN(args)
+        raise NotImplementedError
 
     def create_ffn(self, args: Namespace):
         """
@@ -73,6 +77,34 @@ class MoleculeModel(nn.Module):
 
     def forward(self, *input):
         """
+        Defines the computation performed at every call.
+
+        Should be overridden by all subclasses.
+        """
+        raise NotImplementedError
+
+
+class MoleculeModel(Model):
+    """A MoleculeModel is a model which contains a message passing network following by feed-forward layers."""
+
+    def __init__(self, classification: bool):
+        """
+        Initializes the MoleculeModel.
+
+        :param classification: Whether the model is a classification model.
+        """
+        super(MoleculeModel, self).__init__(classification)
+
+    def create_encoder(self, args: Namespace):
+        """
+        Creates the message passing encoder for the model.
+
+        :param args: Arguments.
+        """
+        self.encoder = MPN(args)
+
+    def forward(self, *input):
+        """
         Runs the MoleculeModel on input.
 
         :param input: Input.
@@ -87,17 +119,71 @@ class MoleculeModel(nn.Module):
         return output
 
 
+class ReactionModel(Model):
+    """
+    A ReactionModel is a model which contains the same message passing
+    network for the reactant and product molecules, followed by the
+    formation of difference features and subsequent encoding with a
+    difference message passing network, followed by feed-forward layers.
+    """
+
+    def __init__(self, classification: bool):
+        """
+        Initializes the ReactionModel.
+
+        :param classification: Whether the model is a classification model.
+        """
+        super(ReactionModel, self).__init__(classification)
+
+    def create_encoder(self, args: Namespace):
+        """
+        Creates the message passing encoders for the model.
+
+        :param args: Arguments.
+        """
+        self.encoder = MPN(args, return_atom_hiddens=True)
+        self.diff_encoder = MPNDiff(args, atom_fdim=args.hidden_size)
+
+    def forward(self,
+                rbatch: Union[List[str], BatchMolGraph],
+                pbatch: Union[List[str], BatchMolGraph],
+                features_batch: List[np.ndarray] = None) -> torch.FloatTensor:
+        """
+        Runs the ReactionModel on input.
+
+        :param rbatch: A list of SMILES strings or a BatchMolGraph for the reactants.
+        :param pbatch: A list of SMILES strings or a BatchMolGraph for the products.
+        :param features_batch: A list of ndarrays containing additional features.
+        :return: The output of the ReactionModel.
+        """
+        r_atom_features = self.encoder(rbatch)
+        p_atom_features = self.encoder(pbatch)
+
+        diff_features = p_atom_features - r_atom_features
+        output = self.ffn(self.diff_encoder(diff_features, pbatch, features_batch))  # use product graph
+
+        # Don't apply sigmoid during training b/c using BCEWithLogitsLoss
+        if self.classification and not self.training:
+            output = self.sigmoid(output)
+
+        return output
+
+
 def build_model(args: Namespace) -> nn.Module:
     """
-    Builds a MoleculeModel, which is a message passing neural network + feed-forward layers.
+    Builds a MoleculeModel or ReactionModel, which is a message passing neural network + feed-forward layers.
 
     :param args: Arguments.
-    :return: A MoleculeModel containing the MPN encoder along with final linear layers with parameters initialized.
+    :return: A MoleculeModel/ReactionModel containing the MPN encoder
+             along with final linear layers with parameters initialized.
     """
     output_size = args.num_tasks
     args.output_size = output_size
 
-    model = MoleculeModel(classification=args.dataset_type == 'classification')
+    if args.reaction:
+        model = ReactionModel(classification=args.dataset_type == 'classification')
+    else:
+        model = MoleculeModel(classification=args.dataset_type == 'classification')
     model.create_encoder(args)
     model.create_ffn(args)
 
