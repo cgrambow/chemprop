f6e95f1126a7cc1a50f072b3b960f6810fad5b30
Colin Grambow <cgrambow@mit.edu>
2019-05-15 21:10:21 -0400

    Modify predict function to also work with reaction data

--- chemprop/train/predict.py
+++ chemprop/train/predict.py
@@ -1,21 +1,21 @@
-from typing import List
+from typing import List, Union
 
 import torch
 import torch.nn as nn
 from tqdm import trange
 
-from chemprop.data import MoleculeDataset, StandardScaler
+from chemprop.data import MoleculeDataset, ReactionDataset, StandardScaler
 
 
 def predict(model: nn.Module,
-            data: MoleculeDataset,
+            data: Union[MoleculeDataset, ReactionDataset],
             batch_size: int,
             scaler: StandardScaler = None) -> List[List[float]]:
     """
     Makes predictions on a dataset using an ensemble of models.
 
     :param model: A model.
-    :param data: A MoleculeDataset.
+    :param data: A MoleculeDataset or ReactionDataset.
     :param batch_size: Batch size.
     :param scaler: A StandardScaler object fit on the training targets.
     :return: A list of lists of predictions. The outer list is examples
@@ -29,14 +29,19 @@ def predict(model: nn.Module,
 
     for i in trange(0, num_iters, iter_step):
         # Prepare batch
-        mol_batch = MoleculeDataset(data[i:i + batch_size])
+        data_batch = data[i:i + batch_size]
+        mol_batch = ReactionDataset(data_batch) if isinstance(data, ReactionDataset) else MoleculeDataset(data_batch)
         smiles_batch, features_batch = mol_batch.smiles(), mol_batch.features()
 
         # Run model
         batch = smiles_batch
 
         with torch.no_grad():
-            batch_preds = model(batch, features_batch)
+            if isinstance(data, ReactionDataset):
+                rbatch, pbatch = list(zip(*batch))
+                batch_preds = model(rbatch, pbatch, features_batch)
+            else:
+                batch_preds = model(batch, features_batch)
 
         batch_preds = batch_preds.data.cpu().numpy()
 
