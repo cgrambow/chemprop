ecddced7c3df2fdb4ebc1ede048b8194cb5c3abf
Colin Grambow <cgrambow@mit.edu>
2019-05-15 21:06:10 -0400

    Modify data loading functions to work with reaction data

--- chemprop/data/utils.py
+++ chemprop/data/utils.py
@@ -3,26 +3,27 @@ import csv
 from logging import Logger
 import pickle
 import random
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Union
 
 import numpy as np
 from tqdm import tqdm
 
-from .data import MoleculeDatapoint, MoleculeDataset
+from .data import MoleculeDatapoint, MoleculeDataset, ReactionDatapoint, ReactionDataset
 from .scaffold import log_scaffold_stats, scaffold_split
 from chemprop.features import load_features
 from chemprop.mol_utils import str_to_mol
 
 
-def get_task_names(path: str, use_compound_names: bool = False) -> List[str]:
+def get_task_names(path: str, use_compound_names: bool = False, reaction: bool = False) -> List[str]:
     """
     Gets the task names from a data CSV file.
 
     :param path: Path to a CSV file.
     :param use_compound_names: Whether file has compound names in addition to smiles strings.
+    :param reaction: Whether file contains reactions instead of molecules.
     :return: A list of task names.
     """
-    index = 2 if use_compound_names else 1
+    index = 1 + use_compound_names + reaction
     task_names = get_header(path)[index:]
 
     return task_names
@@ -68,16 +69,23 @@ def get_smiles(path: str, header: bool = True) -> List[str]:
     return smiles
 
 
-def filter_invalid_smiles(data: MoleculeDataset) -> MoleculeDataset:
+def filter_invalid_smiles(data: Union[MoleculeDataset, ReactionDataset]) -> Union[MoleculeDataset, ReactionDataset]:
     """
     Filters out invalid SMILES.
 
-    :param data: A MoleculeDataset.
-    :return: A MoleculeDataset with only valid molecules.
+    :param data: A MoleculeDataset or ReactionDataset.
+    :return: A MoleculeDataset or ReactionDataset with only valid molecules.
     """
-    return MoleculeDataset([datapoint for datapoint in data
-                            if datapoint.smiles != '' and datapoint.mol is not None
-                            and datapoint.mol.GetNumHeavyAtoms() > 0])
+    if isinstance(data, ReactionDataset):
+        # Allow molecules without heavy atoms
+        return ReactionDataset([datapoint for datapoint in data
+                                if datapoint.rsmiles != '' and datapoint.rmol is not None
+                                if datapoint.psmiles != '' and datapoint.pmol is not None
+                                ])
+    else:
+        return MoleculeDataset([datapoint for datapoint in data
+                                if datapoint.smiles != '' and datapoint.mol is not None
+                                and datapoint.mol.GetNumHeavyAtoms() > 0])
 
 
 def get_data(path: str,
@@ -86,7 +94,7 @@ def get_data(path: str,
              features_path: List[str] = None,
              max_data_size: int = None,
              use_compound_names: bool = None,
-             logger: Logger = None) -> MoleculeDataset:
+             logger: Logger = None) -> Union[MoleculeDataset, ReactionDataset]:
     """
     Gets smiles string and target values (and optionally compound names if provided) from a CSV file.
 
@@ -98,7 +106,7 @@ def get_data(path: str,
     :param max_data_size: The maximum number of data points to load.
     :param use_compound_names: Whether file has compound names in addition to smiles strings.
     :param logger: Logger.
-    :return: A MoleculeDataset containing smiles strings and target values along
+    :return: A MoleculeDataset or ReactionDataset containing smiles strings and target values along
     with other info such as additional features and compound names when desired.
     """
     debug = logger.debug if logger is not None else print
@@ -108,8 +116,10 @@ def get_data(path: str,
         features_path = features_path if features_path is not None else args.features_path
         max_data_size = max_data_size if max_data_size is not None else args.max_data_size
         use_compound_names = use_compound_names if use_compound_names is not None else args.use_compound_names
+        reaction = args.reaction
     else:
         use_compound_names = False
+        reaction = False
 
     max_data_size = max_data_size or float('inf')
 
@@ -131,9 +141,10 @@ def get_data(path: str,
 
         lines = []
         for line in reader:
-            smiles = line[0]
+            rsmiles = line[0]
+            psmiles = line[1] if args.reaction else None
 
-            if smiles in skip_smiles:
+            if rsmiles in skip_smiles or psmiles in skip_smiles:
                 continue
 
             lines.append(line)
@@ -141,14 +152,24 @@ def get_data(path: str,
             if len(lines) >= max_data_size:
                 break
 
-        data = MoleculeDataset([
-            MoleculeDatapoint(
-                line=line,
-                args=args,
-                features=features_data[i] if features_data is not None else None,
-                use_compound_names=use_compound_names
-            ) for i, line in tqdm(enumerate(lines), total=len(lines))
-        ])
+        if reaction:
+            data = ReactionDataset([
+                ReactionDatapoint(
+                    line=line,
+                    args=args,
+                    features=features_data[i] if features_data is not None else None,
+                    use_compound_names=use_compound_names
+                ) for i, line in tqdm(enumerate(lines), total=len(lines))
+            ])
+        else:
+            data = MoleculeDataset([
+                MoleculeDatapoint(
+                    line=line,
+                    args=args,
+                    features=features_data[i] if features_data is not None else None,
+                    use_compound_names=use_compound_names
+                ) for i, line in tqdm(enumerate(lines), total=len(lines))
+            ])
 
     # Filter out invalid SMILES
     if skip_invalid_smiles:
@@ -188,18 +209,17 @@ def get_data_from_smiles(smiles: List[str], skip_invalid_smiles: bool = True, lo
     return data
 
 
-def split_data(data: MoleculeDataset,
+def split_data(data: Union[MoleculeDataset, ReactionDataset],
                split_type: str = 'random',
                sizes: Tuple[float, float, float] = (0.8, 0.1, 0.1),
                seed: int = 0,
                args: Namespace = None,
-               logger: Logger = None) -> Tuple[MoleculeDataset,
-                                               MoleculeDataset,
-                                               MoleculeDataset]:
+               logger: Logger = None) -> Union[Tuple[MoleculeDataset, MoleculeDataset, MoleculeDataset],
+                                               Tuple[ReactionDataset, ReactionDataset, ReactionDataset]]:
     """
     Splits data into training, validation, and test splits.
 
-    :param data: A MoleculeDataset.
+    :param data: A MoleculeDataset or ReactionDataset.
     :param split_type: Split type.
     :param sizes: A length-3 tuple with the proportions of data in the
     train, validation, and test sets.
@@ -252,7 +272,10 @@ def split_data(data: MoleculeDataset,
             train = train_val[:train_size]
             val = train_val[train_size:]
 
-        return MoleculeDataset(train), MoleculeDataset(val), MoleculeDataset(test)
+        if args.reaction:
+            return ReactionDataset(train), ReactionDataset(val), ReactionDataset(test)
+        else:
+            return MoleculeDataset(train), MoleculeDataset(val), MoleculeDataset(test)
     
     elif split_type == 'scaffold_balanced':
         return scaffold_split(data, sizes=sizes, balanced=True, seed=seed, logger=logger)
@@ -267,13 +290,16 @@ def split_data(data: MoleculeDataset,
         val = data[train_size:train_val_size]
         test = data[train_val_size:]
 
-        return MoleculeDataset(train), MoleculeDataset(val), MoleculeDataset(test)
+        if args.reaction:
+            return ReactionDataset(train), ReactionDataset(val), ReactionDataset(test)
+        else:
+            return MoleculeDataset(train), MoleculeDataset(val), MoleculeDataset(test)
 
     else:
         raise ValueError(f'split_type "{split_type}" not supported.')
 
 
-def get_class_sizes(data: MoleculeDataset) -> List[List[float]]:
+def get_class_sizes(data: Union[MoleculeDataset, ReactionDataset]) -> List[List[float]]:
     """
     Determines the proportions of the different classes in the classification dataset.
 
